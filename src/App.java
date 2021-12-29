7a50881c51445bb4fe740463dde481f37afcbd50
Marcelo Machado <marcelo_2016@outlook.com>
2021-12-28 21:51:51 -0300

    Aula4

---
+++ src/App.java
@@ -0,0 +1,6 @@
+/* package primeiroprograma; pacote*/
+public class App {     //Classe primeira maiuscula
+    public static void main(String[] args) throws Exception {   //método
+        System.out.println("Hello, World!"); //sistema saída imprimir
+    }
+}
